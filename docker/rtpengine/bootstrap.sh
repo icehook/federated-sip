161443e32e063ae5a06fe74f9c9917b18d4be72c
Keith Larrimore <keithlarrimore@gmail.com>
2016-02-17 23:27:48 -0500

    updated rtpengine command

--- docker/rtpengine/bootstrap.sh
+++ docker/rtpengine/bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-/usr/local/rtpengine/rtpengine -p /var/run/rtpengine.pid --interface 0.0.0.0 --listen-ng 0.0.0.0:60000 -m 50000 -M 55000 -E -L 3 -f
+/usr/local/rtpengine/rtpengine -p /var/run/rtpengine.pid --interface 0.0.0.0 --listen-ng 0.0.0.0:60000 -m 50000 -M 55000 -E -L 3 -f -q
 
 if [[ $1 == "-d" ]]; then
   tail -f /dev/null
